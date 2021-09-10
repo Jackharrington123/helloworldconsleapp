0cf806ea5f7f984d97ab84f72fcccb0958cb67bb
Jack Harrington <harringtonj@rsd407.org>
2021-09-10 08:10:36 -0700

    Add project files.

---
+++ hello world consle app/hello world consle app.cpp
@@ -0,0 +1,13 @@
+// hello world consle app.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <conio.h>
+
+#include <stdio.h>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
