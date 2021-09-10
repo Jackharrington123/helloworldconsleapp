e51e1edeca84cf34d7c67aba2315f5e78c652d9b
Jack Harrington <harringtonj@rsd407.org>
2021-09-10 08:13:26 -0700

    added my name

--- hello world consle app/hello world consle app.cpp
+++ hello world consle app/hello world consle app.cpp
@@ -1,9 +1,9 @@
 // hello world consle app.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//jack harrington
+
 
 #include <iostream>
 #include <conio.h>
-
 #include <stdio.h>
 
 int main()
